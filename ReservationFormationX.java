c563711048bc1b464c620077ce45ddfa20313d0d
aymanLM7 <152167453+aymanLM7@users.noreply.github.com>
2023-12-01 13:28:03 +0100

    Create ReservationFormationX.java

---
+++ ReservationFormationX.java
@@ -0,0 +1,168 @@
+import java.sql.*;
+import java.util.HashMap;
+
+public class ReservationFormationX {
+    static final String CONN_URL = "jdbc:oracle:thin:@oracle1.ensimag.fr:1521:oracle1";
+    static final String USER = "lmimouna";
+    static final String PASSWD = "lmimouna";
+
+    private Connection conn;
+    private int idRes;
+    private HashMap<Integer,Integer> reservFormationAttente;
+
+    public ReservationFormationX(int idRes, int idUsr, Formation formation){
+        this.idRes = idRes;
+        int annee = formation.getAnnee();
+        int rang = formation.getRang();
+        try {
+            // Enregistrement du driver Oracle
+            System.out.print("Loading Oracle driver... ");
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            System.out.println("loaded");
+
+            // Etablissement de la connection
+            System.out.print("Connecting to the database... ");
+            conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
+            System.out.println("connected");
+            if (verifyAdherent(idUsr)) {
+                this.InitResAttente(annee, rang);
+            }
+            conn.close();
+            return;
+        }   catch (SQLException e) {
+            System.err.println("failed");
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private void InitResAttente(int annee, int rang) throws SQLException {
+        this.reservFormationAttente = new HashMap<Integer, Integer>();
+        String nbResStatement = "SELECT * FROM ReservationFormation WHERE annee = ? AND rang = ? AND rangAttente >= 1";
+        PreparedStatement stmnt = conn.prepareStatement(nbResStatement);
+        stmnt.setInt(1, annee);
+        stmnt.setInt(2, rang);
+        ResultSet result = stmnt.executeQuery();
+
+        while (result.next()) {
+            int retrievedIdRes = result.getInt("idReservationFormation");
+            int retrievedRangAttente = result.getInt("rangAttente");
+
+            this.reservFormationAttente.put(retrievedIdRes, retrievedRangAttente);
+        }
+        stmnt.close();
+        result.close();
+        System.out.println("En Attente = " + this.reservFormationAttente.size());
+    }
+    private int CalculNbRes(int idUsr,int annee, int rang) throws SQLException {
+        String nbResStatement = "SELECT COUNT(*) FROM ReservationFormation WHERE annee = ? AND rang = ? AND rangAttente = 0 AND idUsr = ?";
+        PreparedStatement stmnt = conn.prepareStatement(nbResStatement);
+        stmnt.setInt(1, annee);
+        stmnt.setInt(2, rang);
+        stmnt.setInt(3, idUsr);
+        ResultSet result = stmnt.executeQuery();
+        int res = 0;
+        if(result.next()) {
+            res = result.getInt(1);
+        }
+        stmnt.close();
+        result.close();
+        System.out.println(res);
+        return res;
+    }
+    public void AnnulationResFormation(int idUsr, int annee, int rang) throws SQLException {
+        // Enregistrement du driver Oracle
+        System.out.print("Loading Oracle driver... ");
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        System.out.println("loaded");
+
+        // Etablissement de la connection
+        System.out.print("Connecting to the database... ");
+        conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
+        System.out.println("connected");
+        InitResAttente(annee, rang);
+        int nbRes = this.CalculNbRes(idUsr,annee,rang);
+        if (nbRes > 0) {
+            String deleteStatement = "DELETE FROM ReservationFormation WHERE idReservationFormation = ?";
+            PreparedStatement stmt = conn.prepareStatement(deleteStatement);
+            stmt.setInt(1, this.idRes);
+            ResultSet resultSet = stmt.executeQuery();
+            stmt.close();
+            resultSet.close();
+            String priceStatement = "UPDATE Utilisateur SET SommeDue = SommeDue - ? WHERE idUSr = ?";
+            PreparedStatement stmtPrice = conn.prepareStatement(priceStatement);
+            stmtPrice.setInt(1, this.calculPrix(annee,rang));
+            stmtPrice.setInt(2, idUsr);
+            ResultSet resultSetPrice = stmtPrice.executeQuery();
+            stmtPrice.close();
+            resultSetPrice.close();
+            this.updateReservations(idUsr, annee, rang);
+        } else {
+            System.out.println("L'annulation de cette réservation n'est pas possible.");
+        }
+        conn.close();
+    }
+
+    private int calculPrix(int anneeFormation, int rangFormation) throws SQLException {
+        int prixReservation = 0;
+
+        String prestmnt = "SELECT prixFormation FROM Formation WHERE annee = ? AND rang = ?";
+        PreparedStatement stmnt = conn.prepareStatement(prestmnt);
+        stmnt.setInt(1, anneeFormation);
+        stmnt.setInt(2, rangFormation);
+        ResultSet resultSet = stmnt.executeQuery();
+        if (resultSet.next()) {
+            prixReservation = resultSet.getInt(1);
+            stmnt.close();
+            resultSet.close();
+        }
+        return prixReservation;
+    }
+
+    private boolean verifyAdherent(int idUsr) throws SQLException {
+        String adherentExistenceStatement = "SELECT * FROM Adherent WHERE idUsr = ?";
+        PreparedStatement stmt = conn.prepareStatement((adherentExistenceStatement));
+        stmt.setInt(1, idUsr);
+        ResultSet resultSet = stmt.executeQuery();
+        if(resultSet.next()){
+            stmt.close();
+            resultSet.close();
+            return true;
+        }
+        System.out.println("Vous n'êtes pas un adhérent.");
+        stmt.close();
+        resultSet.close();
+        return false;
+    }
+
+    private void updateReservations(int idUsr, int annee, int rang) throws SQLException {
+        System.out.println("Updating " + this.reservFormationAttente.size());
+        for (HashMap.Entry<Integer, Integer> entry : this.reservFormationAttente.entrySet()) {
+            String deleteStatement = "SELECT * FROM ReservationFormation WHERE idReservationFormation = ?";
+            PreparedStatement stmtAttente = conn.prepareStatement(deleteStatement);
+            stmtAttente.setInt(1, this.idRes);
+            ResultSet resultSetAttente = stmtAttente.executeQuery();
+            int anneeAttente = resultSetAttente.getInt("aneee");
+            int rangAttente = resultSetAttente.getInt("rang");
+            stmtAttente.close();
+            resultSetAttente.close();
+            if( anneeAttente == annee && rangAttente == rang) {
+                int idRes = entry.getKey();
+                int valeurRangAttente = entry.getValue();
+                System.out.println("RangAttente = " + valeurRangAttente);
+                valeurRangAttente--;
+                this.reservFormationAttente.put(idRes, valeurRangAttente);
+                String updtStatement = "UPDATE ReservationFormation SET rangAttente = rangAttente - 1 WHERE annee = ? AND rang = ?";
+                PreparedStatement stmt = conn.prepareStatement((updtStatement));
+                stmt.setInt(1, annee);
+                stmt.setInt(2, rang);
+                ResultSet resultSet = stmt.executeQuery();
+                if (valeurRangAttente == 0) {
+                    System.out.println("idUsr = " + idUsr + " vous êtes passez en liste principale, merci pour votre patience.");
+                    this.reservFormationAttente.remove(idRes);
+                }
+                stmt.close();
+                resultSet.close();
+            }
+        }
+    }
+}
